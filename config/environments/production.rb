72af6fc3dcc8fb18bf0224b4bb8412b0c36d59d7
justin goode <justinmgoode@gmail.com>
2013-05-21 23:00:01 +0200

    Connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Omrails::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # Configuring Amazon S3 for Paperclip file uploads
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
